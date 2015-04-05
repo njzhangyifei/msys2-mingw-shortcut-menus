f3e6b233ca59b0be90805214050eee6f5d0c6c8b
Yifei Zhang <njzhangyifei@gmail.com>
2015-04-04 18:29:17 -0700

    bash should import bash_profile instead of use --init param

--- mingw64_here.bat
+++ mingw64_here.bat
@@ -1,4 +1,4 @@
 @echo off
 @echo Swithching to %*
 SET destdir=%*
-C:\Windows\System32\cmd.exe /U /Q /K C:\Users\Yifei\develop\msys64\mingw32_shell.bat -c 'cd $destdir; exec bash --login'
+C:\Windows\System32\cmd.exe /U /Q /K C:\Users\Yifei\develop\msys64\mingw32_shell.bat -c 'cd $destdir; exec bash --init-file ~/.bash_profile'
