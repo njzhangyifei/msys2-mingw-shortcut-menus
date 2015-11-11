e683f50e4f47cb26fd384fc3bc393f2879be9a26
Yifei Zhang <njzhangyifei@gmail.com>
2015-11-11 11:11:33 -0800

    fully working

--- msys2_here.bat
+++ msys2_here.bat
@@ -1,4 +1,4 @@
 @echo off
 @echo Swithching to %*
 SET destdir=%*
-C:\Windows\System32\cmd.exe /U /Q /K C:\Users\Yifei\msys2\msys2_shell.bat -c 'cd "${destdir}"; exec bash --init-file ~/.bash_profile'
+C:\Windows\System32\cmd.exe /U /Q /K C:\Users\Yifei\msys2\mingw64_shell.bat -c 'cd "${destdir}"; exec bash --init-file ~/.bash_profile'
